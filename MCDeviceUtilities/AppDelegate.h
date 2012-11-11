08b81bc821330767c5d0bcc4abf061f4a5b8bd99
Baglan Dosmagambetov <baglan.dos@gmail.com>
2012-11-11 15:39:14 +0600

    Initial Commit

---
+++ MCDeviceUtilities/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  MCDeviceUtilities
+//
+//  Created by Baglan on 11/11/12.
+//  Copyright (c) 2012 MobileCreators. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
